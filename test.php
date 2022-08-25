65c1a429d5aef29aa13aa82fe207b4769ed9807c
Carranya <k.gong82@gmail.com>
2022-08-25 14:57:04 +0200

    testing some function

--- test.php
+++ test.php
@@ -1,9 +1,16 @@
 <?php
 
-$code = 1234;
-$color = "red";
-$a = "<p style='color: $color'>$code</p>";
-
-echo $a;
+echo "<form action='test2.php' method='post'>";
+for($i=0; $i<4; $i++)
+{
+    echo "<select name='antwort[$i]'>";
+    for($j=1; $j<=4; $j++)
+    {
+        echo "<option value='$j'>$j</option>";
+    }
+    echo "</select>";
+}
+echo "<input type='submit'></input>";
+echo "</form>";
 
 ?>
