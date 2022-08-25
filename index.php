2466a064f00d157ade870d8336c71deb04f6682f
Carranya <k.gong82@gmail.com>
2022-08-25 17:55:11 +0200

    updated index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
 <?php
     class Game
     {
-        function __construct(private $round = 0, private $randomcode){}
+        function __construct(private $round = 0, private $randomcode = 0, private $inputcode = 0){}
 
-        function randomcode()
+        function generate() // generate new hidden code
         {
             $this->randomcode = random_int(1,4);
             for($i=0; $i<3; $i++)
@@ -21,36 +21,118 @@
             return $this->randomcode;
         }
 
-        function anzeigen()
+        function round() // return actual round
         {
-            echo $this->randomcode;
+            $this->round++;
+            return $this->round;
         }
 
+        function randomcode() // return randomcode
+        {
+            return $this->randomcode;
+        }
 
+        function inputcode($answer1, $answer2, $answer3, $answer4)
+        {
+            $this->inputcode = $answer1;
+            $this->inputcode .= $answer2;
+            $this->inputcode .= $answer3;
+            $this->inputcode .= $answer4;
+            return $this->inputcode; 
+        }
+
+        function exam()
+        {
+            for($i=0; $i<4; $i++)
+            {
+                for($j=0; $j<4; $j++)
+                {
+                    if($this->inputcode[$i] == $this->randomcode[$j])
+                    {
+                        if($this->inputcode[$i] == $this->randomcode[$i])
+                        {
+                            echo "Volltreffer 1x<br>";
+                        }
+                        else
+                        {
+                            echo "Fasttreffer 1x<br>";
+                        }
+                    }
+                        else
+                    {
+                        echo "Keine Treffer <br>";
+                    }
+                }
+                echo "<br>";
+            }
+            
+        }
     }
 
-    if(isset($_POST{"answer"}))
+    echo "<form action='index.php' method='post'>";
+    
+
+    if(isset($_POST['answer1']))
     {
-        echo "Antwort gesetzt<br>";
-        $answer =  $_POST['answer'];
-        echo $answer;
-        echo "<br>";
-        echo "Randomcode<br>";
-        $randomcode = new Game(0, $_POST['randomcode']);
-        $randomcode->anzeigen();
+        $game = new Game($_POST['round'], $_POST['randomcode']);
+        //$game = new Game($_POST['round'], 4444);
+        $round = $game->round();
+        $randomcode = $game->randomcode();
+        $inputcode = $game->inputcode($_POST['answer1'], $_POST['answer2'], $_POST['answer3'], $_POST['answer4']);
     }
     else
     {
-        $randomcode = new Game(0,0);
-        $randomcode->randomcode();
-        $randomcode->anzeigen();
-        $senden = $randomcode->randomcode();
-        echo "<form action='index.php' method='post'>";
-        echo "<input name='answer'>";
-        echo "<input type='hidden' name='randomcode' value='$senden'>";
-        echo "<input type='submit' value='antwort'>";
-        echo "</form>";
+        $game = new Game();
+        $round = $game->round();
+        $randomcode = $game->generate();
     }
+
+        echo "<input type='hidden' name='round' value='$round'>";
+        echo "<input type='hidden' name='randomcode' value='$randomcode'>";
+
+        echo "<select name='answer1'>";
+        echo "<option value=1>1</option>";
+        echo "<option value=2>2</option>";
+        echo "<option value=3>3</option>";
+        echo "<option value=4>4</option>";
+        echo "</select>";
+
+        echo "<select name='answer2'>";
+        echo "<option value=1>1</option>";
+        echo "<option value=2>2</option>";
+        echo "<option value=3>3</option>";
+        echo "<option value=4>4</option>";
+        echo "</select>";
+
+        echo "<select name='answer3'>";
+        echo "<option value=1>1</option>";
+        echo "<option value=2>2</option>";
+        echo "<option value=3>3</option>";
+        echo "<option value=4>4</option>";
+        echo "</select>";
+
+        echo "<select name='answer4'>";
+        echo "<option value=1>1</option>";
+        echo "<option value=2>2</option>";
+        echo "<option value=3>3</option>";
+        echo "<option value=4>4</option>";
+        echo "</select>";
+
+        echo "<input type='submit' value='send'>";
+        echo "</form>";
+
+        echo "Input: $inputcode<br>";
+        echo "Random: $randomcode<br>";
+        echo "<br>";
+        $game->exam();
+    
+
+    
+
+
+
+
+
 ?>
 </body>
 </html>
